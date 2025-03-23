27be97c54ab4f7c53d991bf722c226992fa6d022
ldv1970 <ldv1970@gmail.com>
2025-03-23 18:10:06 +0300

    Add link from event to model_grid

--- chdb/che/models.py
+++ chdb/che/models.py
@@ -59,6 +59,7 @@ class Particle(models.Model):
 class Event(models.Model):
     disk = models.ForeignKey(Disk, on_delete=models.DO_NOTHING)
     model_atmosphere = models.ForeignKey(Model_Atmosphere, on_delete=models.DO_NOTHING)
+    model_grid = models.ForeignKey(Model_Grid, on_delete=models.DO_NOTHING)
     model_interaction = models.ForeignKey(Model_Interaction, on_delete=models.DO_NOTHING)
     particle = models.ForeignKey(Particle, on_delete=models.DO_NOTHING)
 

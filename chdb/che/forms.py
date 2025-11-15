7121cd5fb3581a8f4dad1a79c62dd2fb0f2fd4d1
ldv1970 <ldv1970@gmail.com>
2025-11-15 16:51:23 +0300

    Add more filters on histogram page

--- chdb/che/forms.py
+++ chdb/che/forms.py
@@ -1,8 +1,20 @@
 from django import forms
 
 class HistogramForm(forms.Form):
+    energy_from = forms.FloatField(label='Energy from', required=False)
+    energy_to = forms.FloatField(label='Energy to', required=False)
+
     height_from = forms.IntegerField(label='Height from', required=False)
     height_to = forms.IntegerField(label='Height to', required=False)
+
+    phip_from = forms.FloatField(label='Phip from', required=False)
+    phip_to = forms.FloatField(label='Phip to', required=False)
+
+    theta_from = forms.FloatField(label='Theta from', required=False)
+    theta_to = forms.FloatField(label='Theta to', required=False)
+
+    thetap_from = forms.FloatField(label='Thetap from', required=False)
+    thetap_to = forms.FloatField(label='Thetap to', required=False)
 
     COLNAME_CHOICES = [
         ('energy', 'energy'),
--- chdb/che/views.py
+++ chdb/che/views.py
@@ -21,10 +21,32 @@ def histogram(request):
     }
     if form_ok:
         query = Event.objects
+
+        if form.cleaned_data['energy_from'] is not None:
+            query = query.filter(energy__gte=form.cleaned_data['energy_from'])
+        if form.cleaned_data['energy_to'] is not None:
+            query = query.filter(energy__lte=form.cleaned_data['energy_to'])
+
         if form.cleaned_data['height_from'] is not None:
             query = query.filter(height__gte=form.cleaned_data['height_from'])
         if form.cleaned_data['height_to'] is not None:
             query = query.filter(height__lte=form.cleaned_data['height_to'])
+
+        if form.cleaned_data['phip_from'] is not None:
+            query = query.filter(phip__gte=form.cleaned_data['phip_from'])
+        if form.cleaned_data['phip_to'] is not None:
+            query = query.filter(phip__lte=form.cleaned_data['phip_to'])
+
+        if form.cleaned_data['theta_from'] is not None:
+            query = query.filter(theta__gte=form.cleaned_data['theta_from'])
+        if form.cleaned_data['theta_to'] is not None:
+            query = query.filter(theta__lte=form.cleaned_data['theta_to'])
+
+        if form.cleaned_data['thetap_from'] is not None:
+            query = query.filter(thetap__gte=form.cleaned_data['thetap_from'])
+        if form.cleaned_data['thetap_to'] is not None:
+            query = query.filter(thetap__lte=form.cleaned_data['thetap_to'])
+
         pts = list(query.values_list(colname, flat=True))
         if pts:
             fig = go.Figure(
